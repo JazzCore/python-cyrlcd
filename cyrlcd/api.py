43638de3eb8d44141533f4bdbaf1674488161c49
Stanislav Golovanov <stgolovanov@gmail.com>
2013-03-14 13:25:51 +0400

    add docstrings

--- cyrlcd/api.py
+++ cyrlcd/api.py
@@ -3,9 +3,19 @@
 from .cyrlcd import CyrLCD
 
 def str2hex(string):
+    """
+    Convert string to a hex format, "\\xFF" style.
+
+    :param string: string to convert
+    """
     r = CyrLCD()
     return r.conv(string)
 
 def hex2str(string):
+    """
+    Convert hex string in "\xFF" format to a normal string.
+
+    :param string: string to convert
+    """
     r = CyrLCD()
     return r.convback(string)
